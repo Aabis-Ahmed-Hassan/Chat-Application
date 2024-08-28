5193bc351f7f17392f054005e6f13412583ff5e3
Aabis Ahmed Hassan <aabisahmedhassan3@gmail.com>
2024-08-28 17:19:33 +0500

    Done with Login Screen

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chatapplication/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -5,8 +6,75 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('Welcome to Chat App'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: width * 0.05,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                  height: height * .2,
+                  // width: width * .3,
+                  // fit: BoxFit.cover,
+                  'assets/app_icon.png'),
+              SizedBox(
+                height: height * .2,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => HomeScreen(),
+                    ),
+                  );
+                },
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: height * .015),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        height: height * .05,
+                        // width: width * .1,
+                        fit: BoxFit.cover,
+                        'assets/google.png',
+                      ),
+                      SizedBox(
+                        width: width * .02,
+                      ),
+                      Text(
+                        'Sign in with ',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        'Google',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
